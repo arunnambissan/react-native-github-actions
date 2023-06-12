6e22d9bfff5c93670bb762beef63704e629d34b2
Arun D <a.nambissan@gmail.com>
2023-06-12 14:36:18 +0530

    Create test_script.sh

---
+++ scripts/test_script.sh
@@ -0,0 +1,8 @@
+echo "direct access github secret" 
+echo $TEST_GITHUB_SECRET 
+echo "direct access github variable" 
+echo $TEST_GITHUB_VARIABLE 
+echo "access secret using yml env var" 
+echo $TEST_GITHUB_SECRET_VAL 
+echo "access variable using yml env var" 
+echo $TEST_GITHUB_VARIABLE_VAL
